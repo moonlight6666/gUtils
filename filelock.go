b990835943957d1f49a0d43668cdfebdca044032
zrb <>
2024-01-11 10:28:29 +0800

    文件锁

---
+++ filelock.go
@@ -0,0 +1,34 @@
+package gUtils
+
+import (
+	"os"
+	"syscall"
+)
+
+type fileLock struct {
+	f        *os.File
+	filename string
+}
+
+func NewFileLock(file string) (*fileLock, error) {
+	f, err := os.Create(file)
+	if err != nil {
+		return nil, err
+	}
+
+	fl := &fileLock{
+		f:        f,
+		filename: file,
+	}
+	if err = syscall.Flock(int(fl.f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+		return nil, err
+	}
+
+	return fl, nil
+}
+
+func (fl *fileLock) UnlockFile() error {
+	defer fl.f.Close()
+	os.Remove(fl.filename)
+	return syscall.Flock(int(fl.f.Fd()), syscall.LOCK_UN)
+}
