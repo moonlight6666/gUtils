63b0dcdb7203d6758798a680a6d8248c55e385ef
zrb <>
2024-08-17 16:57:19 +0800

    WaitCommandInput

--- wait_command_input.go
+++ wait_command_input.go
@@ -16,14 +16,28 @@ func NewWaitCommandInput() *WaitCommandInput {
 		scanner: bufio.NewScanner(os.Stdin),
 	}
 }
-
 func (w *WaitCommandInput) Wait(name string) string {
+	return w.do(name, "")
+}
+
+func (w *WaitCommandInput) WaitWithDefault(name string, defaultValue string) string {
+	return w.do(name, defaultValue)
+}
+
+func (w *WaitCommandInput) do(name, defaultValue string) string {
 	var input string
 	for {
-		fmt.Printf("请输入%s：", name)
+		if defaultValue == "" {
+			fmt.Printf("请输入%s：", name)
+		} else {
+			fmt.Printf("请输入%s(默认值:%s)：", defaultValue, name)
+		}
 		w.scanner.Scan()
 		input = strings.TrimSpace(w.scanner.Text())
 		if input == "" {
+			if defaultValue != "" {
+				return defaultValue
+			}
 			fmt.Println("请重新输入~")
 			continue
 		}
